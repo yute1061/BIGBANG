1289db7cd75f3845bedf71e2fea208d655cef6fb
yute1061 <siroidanngo1061@yahoo.co.jp>
2024-06-20 16:17:05 +0000

    トップページ_ページング機能追加

--- app/Http/Controllers/NavController.php
+++ app/Http/Controllers/NavController.php
@@ -18,15 +18,45 @@ class NavController extends Controller
     public function toppage(Request $request)
     {   
         $posts = Article::all()->sortByDesc('id');
-        $delete = Article::all()->sortBy('status')->first(); //トップページを開くたびにstatusが1じゃない＝保存だけされて表示されていないデータを削除する
-        
+        //トップページを開くたびにstatusが1じゃない＝DBに保存だけされて表示されていないデータを削除する
+        $delete = Article::all()->sortBy('status')->first();
         if (!empty($delete)) {
             if ($delete->status != 1) {
                 $delete->delete();
             }
         }
         
-        return view('toppage.toppage', ['posts' => $posts]);
+    //----ここからページング処理----
+        //$count_sqlはデータの件数取得に使うための変数。
+        $article_total = Article::count();
+        //ページ数を取得する。GETでページが渡ってこなかった時(最初のページ)のときは$pageに１を格納する。
+        if(isset($_GET['page']) && is_numeric($_GET['page'])) {
+          $page = $_GET['page'];
+        } else {
+          $page = 1;
+        }
+        //最大ページ数を取得する。
+        //10件ずつ表示させているので、$countに入っている件数を10で割って小数点は切りあげると最大ページ数になる。
+        $max_page = ceil($article_total / 10);
+        
+        if($page == 1 || $page == $max_page) {
+            $range = 4;
+        } elseif ($page == 2 || $page == $max_page - 1) {
+            $range = 3;
+        } else {
+            $range = 2;
+        }
+        
+        $from_record = ($page - 1) * 10 + 1;
+        if($page == $max_page && $article_total % 10 !== 0) {
+            $to_record = ($page - 1) * 10 + $article_total % 10;
+        } else {
+            $to_record = $page * 10;
+        }
+    //----ここまで----
+        
+        return view('toppage.toppage', ['posts' => $posts, 'article_total' => $article_total, 'page' => $page, 'max_page' => $max_page, 
+        'range' => $range, 'from_record' => $from_record, 'to_record' => $to_record]);
     }
     
     public function article_list(Request $request)
